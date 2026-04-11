0b9984be68ce6eb386abe29f8dbdd429707468b3
NAButov <rgs-nabutov@pulse.insure>
2026-04-11 18:30:13 +0300

    chore: remove test files

--- _test_import.py
+++
@@ -1,7 +0,0 @@
-import sys
-print('starting', flush=True)
-try:
-    from rtmdk.production.embedding_cache import EmbeddingCache
-    print('EmbeddingCache loaded OK', flush=True)
-except Exception as e:
-    print(f'Error: {e}', flush=True)
